608aeae9f6835ec29dcc247cdbc73af4f8763ee4
Johnk27c <johnk27c@gmail.com>
2024-10-03 20:21:06 -0300

    Clase conexion

--- src/colegio/Colegio.java
+++ src/colegio/Colegio.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package colegio;
+
+import java.sql.Connection;
+import package_Persistencia.Conexion;
 
 /**
  *
@@ -14,7 +14,8 @@ public class Colegio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Connection connect= Conexion.getConexion();
     }
     
 }
--- src/package_Persistencia/Conexion.java
+++ src/package_Persistencia/Conexion.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package package_Persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Jon_kevin27
  */
 public class Conexion {
+    
+    private static final String URL= "jdbc:mariadb://localhost/";
+    private static final String DB= "colegio";
+    private static final String USUARIO= "root";
+    private static final String PASSWORD= "";
+    private static Connection conexion;
+
+    public Conexion() {
+    }
+    
+    public static Connection getConexion (){
+        if (conexion==null){
+            try{
+                
+            Class.forName("org.mariadb.jdbc.Driver");
+            conexion= DriverManager.getConnection(URL+DB, USUARIO, PASSWORD);
+            JOptionPane.showMessageDialog(null, "Conexión exitosa");
+            
+            }catch (ClassNotFoundException e){
+                JOptionPane.showMessageDialog(null, "Error al cargar Drivers");
+            }catch (SQLException e){
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos");
+            }
+        }
+        return conexion;
+    }
+            
     
 }
