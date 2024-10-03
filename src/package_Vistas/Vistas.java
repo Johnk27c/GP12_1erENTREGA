a7bb2fe848e3deb699106a64cf8508a3f110dc89
Johnk27c <johnk27c@gmail.com>
2024-10-03 19:49:56 -0300

    .

---
+++ src/package_Vistas/Vistas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package package_Vistas;
+
+/**
+ *
+ * @author Jon_kevin27
+ */
+public class Vistas {
+    
+}
